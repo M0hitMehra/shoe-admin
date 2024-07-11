0c4f248b5b8d8ac67f41a69c460aa642b708e00d
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2024-07-11 21:44:03 +0530

    dash board page setup

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -13,9 +13,19 @@ import {
 } from "lucide-react";
 import clsx from "clsx";
 import { useRouter } from "next/navigation";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Sidebar = () => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(
+    typeof window !== "undefined" && window.innerWidth >= 768
+  );
 
   const sidebarArray = [
     {
@@ -60,17 +70,47 @@ const Sidebar = () => {
       className={cn(
         " bg-purple-700 h-screen p-5 pt-8 md:w-20 w-16 relative  duration-500",
         {
-          "md:w-72 w-36": open,
+          "md:w-72 w-16": open,
         }
       )}
     >
       <ArrowLeftIcon
         className={cn(
-          " bg-white rounded-full absolute -right-3 cursor-pointer duration-500 border border-purple-700",
-          { " rotate-180": !open }
+          " bg-white rounded-full absolute hidden md:block  -right-3 cursor-pointer duration-500 border border-purple-700",
+          { "  rotate-180": !open }
         )}
         onClick={() => setOpen((prev) => !prev)}
       />
+
+      <Sheet>
+        <SheetTrigger>
+          <ArrowLeftIcon
+            className={cn(
+              " bg-white rounded-full absolute md:hidden block    cursor-pointer duration-500 border border-purple-700",
+              { "  rotate-180": !open }
+            )}
+          />
+        </SheetTrigger>
+        <SheetContent className="bg-purple-700 ">
+          <SheetHeader>
+            {/* <SheetTitle>Are you absolutely sure?</SheetTitle> */}
+            <SheetDescription className=" flex flex-col gap-10 pt-10 justify-between">
+              {sidebarArray?.map((item, i) => (
+                <SmallScreenSidebarTabs
+                  key={i}
+                  icon={item?.icon}
+                  title={item?.title}
+                  state={open}
+                  url={item?.url}
+                  selectedOne={selectedOne}
+                  setSelectedOne={setSelectedOne}
+                />
+              ))}
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+
       <ul className=" flex flex-col gap-10 pt-10 justify-between">
         {sidebarArray?.map((item, i) => (
           <SidebarTabs
@@ -125,7 +165,7 @@ function SidebarTabs({ title, icon, state, url, selectedOne, setSelectedOne }) {
       ) : (
         <li
           className={cn(
-            "flex gap-4 text-neutral-50 font-medium cursor-pointer hover:text-gray-300 group",
+            " hidden md:flex gap-4 text-neutral-50 font-medium cursor-pointer hover:text-gray-300 group",
             {
               "p-3 bg-white rounded-full text-black hover:text-black":
                 selectedOne === url,
@@ -144,5 +184,45 @@ function SidebarTabs({ title, icon, state, url, selectedOne, setSelectedOne }) {
         </li>
       )}
     </>
+  );
+}
+
+function SmallScreenSidebarTabs({
+  title,
+  icon,
+  state,
+  url,
+  selectedOne,
+  setSelectedOne,
+}) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(url);
+    setSelectedOne(url);
+  };
+
+  return (
+    <div>
+      <li
+        className={cn(
+          " flex gap-4 text-neutral-50 font-medium cursor-pointer hover:text-gray-300 group",
+          {
+            "p-3 bg-white rounded-full text-black hover:text-black":
+              selectedOne === url,
+          }
+        )}
+        onClick={handleClick}
+      >
+        <span>{icon}</span>
+        <span
+          className={cn("break-before-all break-words", {
+            block: state,
+          })}
+        >
+          {title}
+        </span>
+      </li>
+    </div>
   );
 }
